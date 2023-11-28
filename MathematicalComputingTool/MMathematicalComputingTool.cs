e8e7e49856e1b12bf743d04f74a0935ef57c32bb
HuyBrSE <151880864+HuyBrSE@users.noreply.github.com>
2023-11-28 20:03:31 +0900

    Add files via upload
    First code

---
+++ MathematicalComputingTool/MMathematicalComputingTool.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalComputingTool
+{
+    public class MMathematicalComputingTool
+    {
+        /// <summary>
+        /// 選択されたテキストファイルのパス
+        /// </summary>
+        public string SelectedTxtPath { get; set; }
+
+        /// <summary>
+        /// 選択されたエクセルファイルのパス
+        /// </summary>
+        public string SelectedExcelPath { get; set; }
+
+        /// <summary>
+        /// 作成されたパス
+        /// </summary>
+        public string CreatedPath { get; set; }
+    }
+
+    public class OutputData
+    {
+        /// <summary>
+        /// 実行時間
+        /// </summary>
+        public string ExecutionTime { get; set; }
+
+        /// <summary>
+        /// 公式数学
+        /// </summary>
+        public string Formulas { get; set; }
+
+        /// <summary>
+        /// 結果
+        /// </summary>
+        public string Results { get; set; }
+
+        /// <summary>
+        /// 参照値
+        /// </summary>
+        public string References { get; set; }
+
+        /// <summary>
+        /// ノート
+        /// </summary>
+        public string Note { get; set; }
+    }
+}
---
+++ MathematicalComputingTool/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace MathematicalComputingTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _viewModel = new VMMathematicalComputingTool();
+            DataContext = this._viewModel;
+        }
+
+        /// <summary>
+        /// データコンテスト
+        /// </summary>
+        private readonly VMMathematicalComputingTool _viewModel;
+
+        /// <summary>
+        /// テキストファイルを選択するボタンがクリックされたときの処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectTxtFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            this._viewModel.SelectFileButton_Click();
+        }
+
+        /// <summary>
+        /// エクセルファイルを選択するボタンがクリックされたときの処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectExcelFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            this._viewModel.SelectFileButton_Click(false);
+        }
+    }
+}
